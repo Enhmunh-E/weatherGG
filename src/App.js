b0332d2b5a3139c15e4e8122ee7cbd224543bae6
enhuush_enhmunh <enhuush0704@gmail.com>
2021-03-19 20:07:08 +0800

    gg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
+import icon_search from './search.png'
 
-function App() {
+const App = () => {
+  const [weather, setWeather] = useState(null); 
+  useEffect(async () => {
+    await axios.get(`https://weather-api.endigo.now.sh/api/v1/weather`)
+      .then(res => {
+          setWeather(res.data.Cities[26]);
+      })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        weather !== null && (
+          <div className="App">
+            <div className="left">
+              <div className='wther'>
+                <p className='gradus'>
+                  {weather.Weathers[0].TemperatureDay}℃
+                </p>
+              </div>
+            </div>
+            <div className="right">
+              <div className="search">
+                <img src={icon_search} width="50px"/>
+              </div>
+              <input type="text" placeholder="Another location"/>
+              <p>City 1</p>
+              <p>City 2</p>
+              <p>City 3</p>
+              <hr/>
+              <p className='h'>Weather Details</p>
+                <div className='tday'>
+                  <p>PhenoDay</p>
+                  <p>{weather.Weathers[0].PhenoDay}</p>
+                </div>
+                <div className='tday'>
+                  <p>PhenoNight</p>
+                  <p>{weather.Weathers[0].PhenoNight}</p>
+                </div>
+                <div className='tday'>
+                  <p>WindDay</p>
+                  <p>{weather.Weathers[0].WindDay}</p>
+                </div>
+                <div className='tday'>
+                  <p>WindNight</p>
+                  <p>{weather.Weathers[0].WindNight}</p>
+                </div>
+            </div>
+        </div>    
+        )
+      }
+    </>
   );
 }
 
